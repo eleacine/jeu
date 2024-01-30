ecb6c9ea578c54edb13ea6709a7cb3518f8bd6f2
Marglou <826marylouhd@gmail.com>
2024-01-30 13:29:35 +0100

    t

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //je croie qu'on a trouver une solutionnnnnnnnn
-        //nouvelle modife
     }
 }
 
