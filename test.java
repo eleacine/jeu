8afc4f93d27438872bcc577134f860ee5cbb5c23
Marglou <826marylouhd@gmail.com>
2024-01-31 13:26:04 +0100

    test

--- test.java
+++ test.java
@@ -1,27 +1,24 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        //je croie qu'on a trouver une solutionnnnnnnnn
+        // je croie qu'on a trouver une solutionnnnnnnnn
         // test 50
-        //hello marylouuu
+        // hello marylouuu
         // //
-        // me parle pas en anglais je répondrais pas  ``
-        //how are you
+        // me parle pas en anglais je répondrais pas ``
+        // how are you
 
-        //M+I = <3
+        // M+I = <3
         // bonjour
-        //comment tu vaas
-        //socejfofbtrecx
-        //voici une blagounette
-        //comment on appele un ytruc qui court et qui se jette
-    //UNE 
-
-
-
-
-
-
+        // comment tu vaas
+        // socejfofbtrecx
+        // voici une blagounette
+        // comment on appele un ytruc qui court et qui se jette
+        // UNE COURGETTE
+        //Marylou? oui? est ce que ton pere est un voleur?Par ce qu'il a voler toutes les etoiles du ciels pour les mettre dans tes yeux
+        //pardon ... jarrete 
+        // y a des moments faut savoir s'arrêter 
+        
 
     }
 }
-
