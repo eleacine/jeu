c65dc20659e45628c831335c8d17ead029887e0c
InesZenati <ineshanna31@gmail.com>
2024-02-19 01:28:43 +0100

    j'ai decomenter du code

--- testEG/Gardien.java
+++ testEG/Gardien.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-class Gardien {
+public class Gardien {
     private int x;
     private int y;
     private int visionAngle;
--- testEG/Joueur.java
+++ testEG/Joueur.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-class Joueur {
+public class Joueur {
     private int x;
     private int y;
 
